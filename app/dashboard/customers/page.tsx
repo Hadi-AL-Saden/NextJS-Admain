15c2395c1e0bae017ca884402d3ec51ac0b68731
unknown <hadialsaden@gmail.com>
2024-01-21 22:01:38 +0300

    First_push

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+
+export default function page() {
+  return (
+    <div><p>Customers Page</p></div>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+
+export default function page() {
+  return (
+    <div><p>Invoices Page</p></div>
+  )
+}
