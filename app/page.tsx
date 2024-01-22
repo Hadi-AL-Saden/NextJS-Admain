14869c5cf8786b7388b452de1d2af06dabd0eb69
unknown <hadialsaden@gmail.com>
2024-01-22 17:58:28 +0300

    test deploying

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-purple-400 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-test-400 p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-5 flex grow flex-col gap-4 md:flex-row">
